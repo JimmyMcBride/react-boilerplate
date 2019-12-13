35f47122bffce75a08c5a90b166d959155240318
JimmyMcBride <mcbride967@gmail.com>
2019-12-13 11:46:03 -0600

    a thing

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 // Import css files for most global settings
 // Sets HTML font-size to 2vw making everything responsive (Use rem instead of pixles now)
 import "./index.css";
-
 // Normalizes our css among different browsers
 import "normalize.css";
 
-import App from "./App";
-
-// Keep this puppy here for later
+// Keep this puppy here for later!
 import * as serviceWorker from "./serviceWorker";
 
+import { applyMiddleware, createStore } from "redux";
+
+import App from "./App";
+import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
 // Set up Redux/Router
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-
+import logger from "redux-logger";
 // Import reducer/index.js as root reducer, it's where we're combining all our reducer files
 import rootReducer from "./store/reducers";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
